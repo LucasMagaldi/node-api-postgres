b06ec4f8b555fe63e7846d2c6ed64256c8b9e6ad
LucasMagaldi <lucas.magaldi@hotmail.com>
2024-01-21 17:46:14 -0300

    feat: hash user password

--- src/http/controllers/User/index.ts
+++ src/http/controllers/User/index.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { prisma } from '../../../connectors/prisma';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { hash } from 'bcryptjs';
 
 export async function register (request: FastifyRequest, response: FastifyReply) {
     const registerBodySchema = z.object({
@@ -11,11 +12,21 @@ export async function register (request: FastifyRequest, response: FastifyReply)
 
     const { name, email, password } = registerBodySchema.parse(request.body);
 
+    const emailAlredyExist = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if(emailAlredyExist) return response.status(409).send();
+
+    const passwordHash = await hash(password,6);
+
     await prisma.user.create({
         data: {
             name,
             email,
-            password
+            password: passwordHash
         }
     });
 
