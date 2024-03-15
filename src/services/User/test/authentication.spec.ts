d7c31c7bfd1cfa5f6edf0ebaa835844d194ee018
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-03-15 01:48:22 -0300

    add unit test to cover authentication scenarios

--- src/services/User/test/authentication.spec.ts
+++ src/services/User/test/authentication.spec.ts
@@ -1,15 +1,17 @@
-import { beforeEach, describe, expect, it } from 'vitest';
-import { UserService } from '../index';
-import { InMemoryUsersRepository } from '../../../repositories/in-memory/in-memory-users-repository';
+import { beforeEach, describe, expect, it } from 'vitest'
+import { UserService } from '../index'
+import { InMemoryUsersRepository } from '../../../repositories/in-memory/in-memory-users-repository'
+import { InValidCredentials } from '../../Errors/InvalidCredentials'
+import { hash } from 'bcryptjs'
 
-let usersRepository: InMemoryUsersRepository;
+let usersRepository: InMemoryUsersRepository
 let sut: UserService; // System under test
 
 describe('Authenticate use case - service', () => {
 
     beforeEach(() => {
-        usersRepository = new InMemoryUsersRepository();
-        sut = new UserService(usersRepository);
+        usersRepository = new InMemoryUsersRepository()
+        sut = new UserService(usersRepository)
     });
     
 
@@ -18,7 +20,7 @@ describe('Authenticate use case - service', () => {
             name: 'Lucas',
             email: 'lucas.magaldddi@hotmail.com', //Every time it be executed change the E-mail
             password: '123456'
-        });
+        })
 
         const { 
             user_id, 
@@ -30,6 +32,30 @@ describe('Authenticate use case - service', () => {
             password: '123456'
         });
 
-        expect(user_id).toEqual(expect.any(String));
-    });
+        expect(user_id).toEqual(expect.any(String))
+    })
+
+    it('Should not be able to authenticate with wrong password', async () => {
+        await usersRepository.create({
+            name: "Lucas Magaldi", 
+            email: "lucas@hotmail.com",
+            password: await hash("123456", 6)
+        })
+
+        expect(() =>
+        sut.authentication({
+            email: 'johndoe@example.com',
+            password: '123123',
+        }),
+        ).rejects.toBeInstanceOf(InValidCredentials)
+    })
+
+    it('Should not be able to authenticate with wrong e-mail', async () => {
+        expect(() =>
+        sut.authentication({
+            email: 'lucas@hotmail.com"',
+            password: '123456',
+        }),
+        ).rejects.toBeInstanceOf(InValidCredentials)
+    })
 })
